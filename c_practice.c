7f03f1a056dcd842b1eaadbd37c55d0efbf909a8
mayanknaughtiyal <nautiyalmamta198@gmail.com>
2024-11-07 16:40:31 +0530

    Create c_practice.c

---
+++ c_practice.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main()
+{  printf("hokoello");
+ /* get started with c
+now first of all there are majorly 3 datatypes  int float and char
+*/ 
+//int float char
+const float c=10.0;
+//some float operations
+double a=4.51;//doubles float precision upto 15,where normal float declaration is upto 6 decimal precision
+long double bk=45.641545;//upto 19 decimal places precision
+char bj_jg ='f';//char can be only one if there are one then more characters then you have to use double qutes"" and it will be called a string or an array of char
+unsigned char y='j';
+signed char j='j';
+const int c=45; //declaraton plus intialisation
+//below are some int functions
+unsigned int g=55;//when we know that our integer will not be negative
+long int h=4445;//when integer is long 
+short int gg=545;//when int is short 
+
+// you can use them like unsigned sort int gh=5; also 
+printf("\n%f\n",a);
+printf("%d\n",c);
+printf("%c\n",bj_jg);
+//to calculate the size of any datatype in an artitechture type
+printf(sizeof(char));
+//
+//
+//types of operators in c
+//arthimetic
+//logical 
+//bitwise
+//assignment
+//miscellaneous
+/*arthimetic opertors
+*/
+    return 0;
+} 
